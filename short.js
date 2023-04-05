d1b58fca803e262b77bb6ae0a5fc068ecd5de3f1
V3rmillionCreator <104515059+V3rmillionCreator@users.noreply.github.com>
2023-04-05 13:51:52 -0500

    Create short.js

---
+++ short.js
@@ -0,0 +1,13 @@
+//copy this if you dont want to copy the string
+fetch('https://cdn.jsdelivr.net/gh/V3rmillionCreator/JSCLI@main/main.js').then(function(response) { 
+        if (!response.ok) { 
+            return false; 
+        } 
+        return response.blob(); 
+    }) .then(function(myBlob) { 
+        var objectURL = URL.createObjectURL(myBlob); 
+        var sc = document.createElement("script");
+        sc.setAttribute("src", objectURL); 
+        sc.setAttribute("type", "text/javascript"); 
+        document.head.appendChild(sc);
+    })
